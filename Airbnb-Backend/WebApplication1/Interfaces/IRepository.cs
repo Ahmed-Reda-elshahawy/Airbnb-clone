fffd0177c736163c8dd434f328a3130822c240dc
Mohamed Aboseif <midozenelelime@gmail.com>
2025-04-08 01:59:11 +0200

    Generic interface, Generic Repository, and user Controller

---
+++ Airbnb-Backend/WebApplication1/Interfaces/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.Blazor;
+
+namespace WebApplication1.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        T GetByID(int id);
+        void Create(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        void Save();
+    }
+}
