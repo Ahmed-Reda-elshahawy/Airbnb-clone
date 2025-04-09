eb6cf8413c7ed8b56018049ee15d73aae848f99b
Fatma Alghonaimy <ffatmaalaa32@gmail.com>
2025-04-09 08:32:49 +0200

    DTOs, Mappers, Listing Crud

---
+++ Airbnb-Backend/WebApplication1/Repositories/ListingsRepository.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace WebApplication1.Repositories
+{
+    public class ListingsRepository:GenericRepository<Listing>
+    {
+        private readonly AirbnbDBContext context;
+        private readonly IMapper mapper;
+
+        public ListingsRepository(AirbnbDBContext _context, IMapper _mapper):base(_context,_mapper)
+        {
+            context = _context;
+            mapper = _mapper;
+        }
+        public async Task<List<Listing>> GetListingsByHostAsync(Guid hostId)
+        {
+            return await context.Set<Listing>()
+                                 .Where(l => l.HostId == hostId) 
+                                 .ToListAsync(); 
+        }
+
+    }
+}
