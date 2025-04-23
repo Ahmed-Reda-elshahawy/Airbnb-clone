d93019a28e9f08fc3a7101d65703e39c1791beec
Fatma Alghonaimy <ffatmaalaa32@gmail.com>
2025-04-23 10:12:15 +0200

    Delete Multiple

--- Airbnb-Backend/WebApplication1/Interfaces/IRepository.cs
+++ Airbnb-Backend/WebApplication1/Interfaces/IRepository.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.Blazor;
 using System.Linq.Expressions;
+using System.Threading.Tasks;
 
 namespace WebApplication1.Interfaces
 {
@@ -13,6 +14,7 @@ namespace WebApplication1.Interfaces
         Task CreateAsync<T>(T entity) where T : class;
         Task<T> UpdateAsync<T, TDto>(Guid id, TDto updateDto) where T : class;
         Task DeleteAsync<T>(Guid id) where T : class;
+        Task DeleteMultipleAsync<T>(Guid[] ids) where T : class;
         Task<T> GetByIDAsync(Guid id, List<string> includeProperties = null);
         Task SaveChangesAsync();
         #endregion
